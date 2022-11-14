2c9ffb259889a9bbc60c466b6c15d9e0315c9cec
AudeMa <aude.masse@hotmail.fr>
2022-11-14 14:37:58 +0100

    Correction

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ jourh.innerHTML = `${day} ${hour}:${minutes}`;
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
 
